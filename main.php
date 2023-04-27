e311cb2721bde1f358f4c1d9d6eef86488749571
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2023-04-27 21:14:28 +0800

    main page css

--- main.php
+++ main.php
@@ -2,10 +2,10 @@
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: login.php');
-	exit;
-}
+// if (!isset($_SESSION['loggedin'])) {
+// 	header('Location: login.php');
+// 	exit;
+// }
 ?>
 
 <!DOCTYPE html>
@@ -13,21 +13,34 @@ if (!isset($_SESSION['loggedin'])) {
 	<head>
 		<meta charset="utf-8">
 		<title>Home Page</title>
-		<link href="css\mainStyle.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+        <!--Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+		<link href="css\mainPageStyle.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Website Title</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="includes\logout.inc.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
+	<body>
+    <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <!--Navbar Logo-->
+                  <a class="navbar-brand" href="#">
+                    <img src="assets\Logo.png" alt="Logo" width="55" height="55">
+                  </a>
+                    <!--Navbar Name-->
+                  <a class="navbar-brand" href="#" style = "color: #D4D4D4;">GuildArts</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                    <a href="profile.php" style="color: #A65A28;text-decoration:none"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="includes\logout.inc.php" style="color: #A65A28;text-decoration:none"><i class="fas fa-sign-out-alt" ></i>Logout</a>
+                    
+                  </div>
+                </div>
+              </nav>
 		<div class="content">
-			<h2>Home Page</h2>
-			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+
+			<p>Welcome back to the Guild, <?=$_SESSION['name']?>!</p>
 		</div>
 	</body>
 </html>
