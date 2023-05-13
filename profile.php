b502e2a1297ac5539889336c9815213a4cb22bc4
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2023-05-13 21:37:28 +0800

    Bug in Css resolved

--- profile.php
+++ profile.php
@@ -32,7 +32,7 @@ $stmt->close();
         <!--Bootstrap-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-		<link href="css\profileStyle.css" rel="stylesheet" type="text/css">
+		<link href="css\profStyle.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body>
@@ -93,24 +93,39 @@ $stmt->close();
             </div>
 </div>
 </div>
+  <section class = "gallery-links">
+    <div class ="wrapper">
+      <h2>Gallery<h2>
+        <div class = "gallery-container">
+          <a href ="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+            </a>
+            <a href ="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+            </a>
+            <a href ="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+            </a>
+            <a href ="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+            </a>
+            <a href ="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+            </a>
+        </div>
+  </section>
 
-<div class="gallerydiv">
-<?php
-    $dir ="gallery/"; // image folder name
-      if (is_dir($dir)){
-         if ($dh = opendir($dir)){
-                 while (($file = readdir($dh)) !== false){
-                    if($file=="." OR $file==".."){} else { 
 
-              ?>   <!---- its a loop [change the folder name on img path]----->                
-                         <img  style="width: 400px;padding:30px" src="gallery/<?php echo $file; ?>"> 
-             <?php
-              }
-             }
-         closedir($dh);
-         }
-      } ?>
-</div>
 
 	</body>
 </html>
