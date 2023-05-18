f74fc688e252a419fe528e2a4880df609bf46c7c
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2023-05-18 20:52:50 +0800

    clickable button

--- main.php
+++ main.php
@@ -76,20 +76,21 @@ if (!isset($_SESSION['loggedin'])) {
                 </div>
               </nav>
               <center>
-                
-              <button class="buttonPost">
-                <a href ="uploadGallery.php">
-                <div class="svg-wrapper-1">
-                  <div class="svg-wrapper">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                      <path fill="none" d="M0 0h24v24H0z"></path>
-                      <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-                    </svg>
+
+              <a href="uploadGallery.php" class="button-link">
+                <button class="buttonPost">
+                  <div class="svg-wrapper-1">
+                    <div class="svg-wrapper">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+                      </svg>
+                    </div>
                   </div>
-                </div>
+                  <span style="color:#D4D4D4;">Create Post</span>
+                </button>
               </a>
-                <span><a href="uploadGallery.php" style="color:#D4D4D4;"> Create Post</a></span>
-              </button>
+
 
               
             </center>
