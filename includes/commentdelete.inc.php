fa3f12e6f863c1d9937cf76b863eb57a03e8f501
Fhourze <124254334+Fhourze@users.noreply.github.com>
2023-05-22 20:32:34 +0800

    better crude design

--- includes/commentdelete.inc.php
+++ includes/commentdelete.inc.php
@@ -50,12 +50,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     <title>Delete Comment</title>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper {
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
+    <link href="..\css\crudeStyle.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -63,9 +58,10 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
+                    <center>
                     <h2 class="mt-5 mb-3">Delete Post</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger">
+
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>" />
                             <p>Are you sure you want to delete this resource?</p>
                             <p>
@@ -74,7 +70,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
 
                             </p>
                             </p>
-                        </div>
+</center>
                     </form>
                 </div>
             </div>
